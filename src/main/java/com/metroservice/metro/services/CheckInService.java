5679572d005cf97b7e4055d1dc801a0fe1038ed2
Varun <2003varunp@gmail.com>
2025-02-13 21:57:25 +0530

    Remove unused import from CheckInService

--- src/main/java/com/metroservice/metro/services/CheckInService.java
+++ src/main/java/com/metroservice/metro/services/CheckInService.java
@@ -3,7 +3,6 @@ package com.metroservice.metro.services;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import org.hibernate.annotations.Check;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
