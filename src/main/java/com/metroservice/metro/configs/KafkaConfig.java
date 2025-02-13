020516a277529a12878fae500b882f9e45d2f8de
Varun <2003varunp@gmail.com>
2025-02-13 16:46:59 +0530

    Add Kafka and Redis configuration classes for messaging and caching

---
+++ src/main/java/com/metroservice/metro/configs/KafkaConfig.java
@@ -0,0 +1,18 @@
+package com.metroservice.metro.configs;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    NewTopic sosAlertTopic() {
+        return TopicBuilder.name("sos-alerts")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+}
